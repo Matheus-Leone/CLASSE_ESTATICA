76667f883542ca8526f6d0bf4e0ede70ff036c9a
Matheus-Leone <matheusleoneevangelista@gmail.com>
2024-10-24 00:21:13 -0300

    Primeiro Commit

--- Classe_Estatica/src/principal/Main.java
+++ Classe_Estatica/src/principal/Main.java
@@ -1,10 +1,12 @@
 package principal;
 
+//Importa para puxar toda a lógica e os cálculos 
 import conversorUnidade.ConversordeUnidades;
 
+//Classe main só para instanciar os métodos
 public class Main {
 
-	
+//O valor "50" é um exemplo, é possível trocá-lo, e após isso é só trocar o valor da string para manter o sentido da frase	
 	public static void main(String[] args) {
 	    
 	    double conversaoTemp = ConversordeUnidades.celsiusParaFarenheit(50);
